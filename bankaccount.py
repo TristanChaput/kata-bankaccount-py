78b93dfd4b50ba63ab4008658d875bdcacb24414
Tristan Chaput <tristan.chaput@arolla.fr>
2022-09-19 12:37:55 +0200

    feat(withdraw)

---
+++ bankaccount.py
@@ -0,0 +1,11 @@
+class BankAccount:
+
+    def __init__(self, iban, balance):
+        self._iban = iban
+        self._balance = balance
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def get_balance(self):
+        return self._balance
